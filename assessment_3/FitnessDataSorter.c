62cc3dc1bb96e9802d6e5eee321c05ea271f93fc
gsleeds <146739194+gsleeds@users.noreply.github.com>
2023-12-11 16:04:48 +0000

    half

---
+++ assessment_3/FitnessDataSorter.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Define the struct for the fitness data
+typedef struct {
+    char date[11];
+    char time[6];
+    int steps;
+} FitnessData;
+
+// Function to tokenize a record
+void tokeniseRecord(char *record, char delimiter, char *date, char *time, int *steps) {
+    char *ptr = strtok(record, &delimiter);
+    if (ptr != NULL) {
+        strcpy(date, ptr);
+        ptr = strtok(NULL, &delimiter);
+        if (ptr != NULL) {
+            strcpy(time, ptr);
+            ptr = strtok(NULL, &delimiter);
+            if (ptr != NULL) {
+                *steps = atoi(ptr);
+            }
+        }
+    }
+}
+
+
+int main() {
+    char filename[100];
+    printf("Enter filename: ");
+    scanf("%99s", filename);
+
+    FILE *file = fopen(filename, "r");
+    if (file == NULL){
+        printf("Error: invalid file");
+        return 1;
+    }
+    
+    FitnessData data[100];
+    char line[100];
+    int count = 0;
+    int invalidFile = 0;
+
+    while(fgets(line, sizeof(line), file)){
+
+        line[strcspn(line,"\n")]=0;
+
+        FitnessData temp;
+        tokeniseRecord(line, ',', temp.date, temp.time, &temp.steps);
+
+        if (strlen(temp.date) != 10 || strlen(temp.time) != 5 || temp.steps <= 0) {
+            invalidFile = 1; // Invalid line
+        }
+        else{
+            data[count++] = temp;
+        }
+    }
+    fclose(file);
+
+    if(invalidFile){
+        printf("Error: invalid file format");
+        return 1;
+    }
+
+}
